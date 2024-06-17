40a06ed134c4860d1632a1cd4fe476e58f4b2ef2
Bogdan Guranda <bogdanguranda@gmail.com>
2024-06-17 12:23:17 +0300

    Added Go mod support; basic visualization

--- oci-analyzer.go
+++ oci-analyzer.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"encoding/json"
+<<<<<<< HEAD
 	"flag"
 	"fmt"
 	"os"
 	"regexp"
 	"strings"
+=======
+	"fmt"
+	"net/url"
+	"os"
+	"regexp"
+	"sort"
+	"strings"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+>>>>>>> f6212a6 (Added Go mod support; basic visualization)
 )
 
 type SyftJSON struct {
@@ -20,6 +33,7 @@ type SyftJSON struct {
 	} `json:"manifests"`
 }
 
+<<<<<<< HEAD
 type DiveJSON struct {
 	Layers []struct {
 		Index     int    `json:"index"`
@@ -59,12 +73,36 @@ func main() {
 		}
 
 		diveData, err := os.ReadFile(diveFilesSlc[idx])
+=======
+type Stats struct {
+	BaseOS   map[string]int
+	Packages map[string]int
+	Runtimes map[string]int
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: syft-analyzer <syft_output_file1.json> ... <syft_output_fileN.json>")
+		os.Exit(1)
+	}
+
+	// Aggregate information from Syft
+	stats := Stats{
+		BaseOS:   make(map[string]int),
+		Packages: make(map[string]int),
+		Runtimes: make(map[string]int),
+	}
+
+	for _, file := range os.Args[1:] {
+		data, err := os.ReadFile(file)
+>>>>>>> f6212a6 (Added Go mod support; basic visualization)
 		if err != nil {
 			fmt.Printf("Error reading file: %v\n", err)
 			os.Exit(1)
 		}
 
 		var syftOutput SyftJSON
+<<<<<<< HEAD
 		err = json.Unmarshal(syftData, &syftOutput)
 		if err != nil {
 			fmt.Printf("Error parsing syft JSON: %v\n", err)
@@ -139,15 +177,89 @@ func extractOSNameAndVersion(distro string) (osName, version string) {
 func detectLanguageAndRuntime(pkgURL string, languages map[string]int, runtimes map[string]int) {
 	if strings.Contains(pkgURL, "pkg:generic/java/") || strings.Contains(pkgURL, "pkg:maven/") {
 		languages["Java"]++
+=======
+		err = json.Unmarshal(data, &syftOutput)
+		if err != nil {
+			fmt.Printf("Error parsing JSON: %v\n", err)
+			os.Exit(1)
+		}
+
+		osNameWithVersion := extractOSNameWithVersion(syftOutput.Metadata.Distro)
+		stats.BaseOS[osNameWithVersion]++
+
+		for _, manifest := range syftOutput.Manifests {
+			for _, pkg := range manifest.Resolved {
+				stats.Packages[extractPackageName(pkg.PackageURL)]++
+				detectRuntime(pkg.PackageURL, stats.Runtimes)
+			}
+		}
+	}
+	printMapNicely(stats.Packages)
+
+	// Generate statistics
+	duplicatePackages := getDuplicates(stats.Packages)
+	printMapNicely(duplicatePackages)
+	if err := generateStats(duplicatePackages, "Package Statistics", "stats_packages.png", 10); err != nil {
+		fmt.Printf("Error generating bar chart for packages: %v\n", err)
+		os.Exit(1)
+	}
+
+	duplicateRuntimes := getDuplicates(stats.Runtimes)
+	printMapNicely(duplicateRuntimes)
+	if err := generateStats(duplicateRuntimes, "Runtime Statistics", "stats_runtime.png", 10); err != nil {
+		fmt.Printf("Error generating bar chart for runtimes: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Finished successfully.")
+}
+
+func printMapNicely(entries map[string]int) {
+	fmt.Println("Entries print:")
+	fmt.Println(strings.Repeat("-", 40))
+
+	for key, count := range entries {
+		fmt.Printf("%-30s\t%d\n", key, count)
+	}
+}
+
+func getDuplicates(entries map[string]int) map[string]int {
+	duplicates := make(map[string]int)
+	for key, count := range entries {
+		if count >= 2 {
+			duplicates[key] = count
+		}
+	}
+	return duplicates
+}
+
+func extractOSNameWithVersion(distro string) (osNameWithVersion string) {
+	re := regexp.MustCompile(`pkg:generic/([^@]+)@([^?]+)`)
+	matches := re.FindStringSubmatch(distro)
+	if len(matches) > 2 {
+		osName := matches[1]
+		version := matches[2]
+		osNameWithVersion = osName + ":" + version
+	}
+	return osNameWithVersion
+}
+
+func detectRuntime(pkgURL string, runtimes map[string]int) {
+	if strings.Contains(pkgURL, "pkg:generic/java/") || strings.Contains(pkgURL, "pkg:maven/") {
+>>>>>>> f6212a6 (Added Go mod support; basic visualization)
 		if strings.Contains(pkgURL, "jre") || strings.Contains(pkgURL, "jdk") {
 			runtimes["Java"]++
 		}
 	} else if strings.Contains(pkgURL, "pkg:python/") {
+<<<<<<< HEAD
 		languages["Python"]++
+=======
+>>>>>>> f6212a6 (Added Go mod support; basic visualization)
 		if strings.Contains(pkgURL, "cpython") {
 			runtimes["Python"]++
 		}
 	}
+<<<<<<< HEAD
 	// TODO: Add more language and runtime detection rules as needed
 }
 
@@ -177,4 +289,76 @@ func convertBytesToHumanReadableString(sizeBytes int64) string {
 	}
 
 	return fmt.Sprintf("%.2f%s", size, units[i])
+=======
+	// TODO: Add more runtime detection rules as needed, e.g. Golang, Node.js, C/C++, C# ...
+}
+
+func extractPackageName(pkgURL string) string {
+	parts := strings.Split(pkgURL, "/")
+	nameWithVersionRaw := parts[len(parts)-1]
+	nameWithVersionFullUrlEncoded := strings.Split(nameWithVersionRaw, "?")[0]
+
+	nameWithVersionUrlDecoded, err := url.QueryUnescape(nameWithVersionFullUrlEncoded)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: Unable to decode package URL: %s\n", pkgURL)
+		return ""
+	}
+
+	return nameWithVersionUrlDecoded
+}
+
+func generateStats(stats map[string]int, title string, filename string, topN int) error {
+	p := plot.New()
+	p.Title.Text = title
+
+	// Create a slice of struct to sort map entries by value
+	type kv struct {
+		Key   string
+		Value int
+	}
+	var ss []kv
+	for k, v := range stats {
+		ss = append(ss, kv{k, v})
+	}
+
+	// Sort the slice by value, and then by key if values are equal
+	sort.Slice(ss, func(i, j int) bool {
+		if ss[i].Value == ss[j].Value {
+			return ss[i].Key < ss[j].Key
+		}
+		return ss[i].Value > ss[j].Value
+	})
+
+	// If topN is greater than the number of stats, adjust topN to the number of stats
+	if topN > len(ss) {
+		topN = len(ss)
+	}
+
+	// Take only the top N entries for the chart
+	ss = ss[:topN]
+
+	// Create the bars and labels for the top N entries
+	bars := make(plotter.Values, topN)
+	labels := make([]string, topN)
+	for i := 0; i < topN; i++ {
+		bars[i] = float64(ss[i].Value)
+		labels[i] = ss[i].Key
+	}
+
+	bar, err := plotter.NewBarChart(bars, vg.Points(20))
+	if err != nil {
+		return err
+	}
+	bar.Horizontal = true // Make the bar chart horizontal
+
+	p.Add(bar)
+	p.NominalY(labels...)
+
+	// Save the plot to a PNG file.
+	if err := p.Save(6*vg.Inch, vg.Length(float64(topN)*0.5)*vg.Inch, filename); err != nil {
+		return err
+	}
+
+	return nil
+>>>>>>> f6212a6 (Added Go mod support; basic visualization)
 }
